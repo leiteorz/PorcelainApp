9d6ba044498613a67f3ef7ce3d638f074972adc1
‘leiteorz’ <2442075815@qq.com>
2023-03-21 18:22:54 +0800

    馆藏列表画好了

---
+++ samples/ar-model-viewer/src/main/java/com/android/china/adpter/GuanCangAdapter.java
@@ -0,0 +1,60 @@
+package com.android.china.adpter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.china.model.GuanCang;
+import com.google.android.material.card.MaterialCardView;
+import com.google.ar.sceneform.samples.gltf.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GuanCangAdapter extends RecyclerView.Adapter<GuanCangAdapter.ViewHolder> {
+    private List<GuanCang> mList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView guanCangImage;
+        TextView guanCangName;
+        TextView guanCangDescription;
+        MaterialCardView guanCangItem;
+
+        public ViewHolder(View view){
+            super(view);
+
+            guanCangImage = view.findViewById(R.id.guan_cang_image);
+            guanCangName = view.findViewById(R.id.guan_cang_name);
+            guanCangDescription = view.findViewById(R.id.guan_cang_description);
+            guanCangItem = view.findViewById(R.id.guan_cang_item);
+        }
+    }
+
+    public GuanCangAdapter(List<GuanCang> guanCangList){
+        mList = guanCangList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_guan_cang_item,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        GuanCang guanCang = mList.get(position);
+        holder.guanCangName.setText(guanCang.getName());
+        holder.guanCangDescription.setText(guanCang.getDescription());
+        holder.guanCangImage.setImageResource(guanCang.getImageId());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+}
---
+++ samples/ar-model-viewer/src/main/java/com/android/china/model/GuanCang.java
@@ -0,0 +1,48 @@
+package com.android.china.model;
+
+import com.android.china.adpter.GuanCangAdapter;
+
+public class GuanCang {
+    int id = -1;
+    String name;    //名字
+    String description; //描述
+    int imageId;    //图片
+
+    public GuanCang(String _name, String _description, int _imageId){
+        this.name = _name;
+        this.description = _description;
+        this.imageId = _imageId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
